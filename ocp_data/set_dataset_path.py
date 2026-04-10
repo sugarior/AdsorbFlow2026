5d12b10dc59a77fdb3d391da1b18f9991eb7e623
zjj <37220232203941@stu.xmu.edu.cn>
2026-04-10 08:35:13 +0800

    ood配置

--- ocp_data/set_dataset_path.py
+++ ocp_data/set_dataset_path.py
@@ -6,7 +6,7 @@ def set_path(args):
         return
     elif args.dataset == 'oc22':
         args.train_src = '/sda1/zzwang/decopm3/oc22data/is2res_total_train_val_test_lmdbs/data/oc22/is2re-total/train'
-        args.val_src = '/sda1/zzwang/decopm3/oc22data/is2res_total_train_val_test_lmdbs/data/oc22/is2re-total/val_id'
+        args.val_src = '/sda1/zzwang/decopm3/oc22data/is2res_total_train_val_test_lmdbs/data/oc22/is2re-total/val_ood'
         args.test_src = '/sda1/zzwang/decopm3/oc22data/is2res_total_train_val_test_lmdbs/data/oc22/is2re-total/test_id'
     else:
         raise ValueError(f"Unsupported dataset: {args.dataset}")
